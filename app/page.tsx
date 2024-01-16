adc62faca58f02ffea9517925ba5fba9126ed45c
codewithbersh <121822380+codewithbersh@users.noreply.github.com>
2024-01-16 13:55:24 +0300

    feat: add font

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
 import { Button } from "@/components/ui/button";
 
 const HomePage = () => {
-  return <Button>HomePage</Button>;
+  return (
+    <div>
+      <h1 className=" font-serif font-bold text-2xl italic">
+        This is IBM PLEX
+      </h1>
+      <h1 className=" font-sans font-bold text-2xl italic">This is Inter</h1>
+    </div>
+  );
 };
 
 export default HomePage;
